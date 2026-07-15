31ccaf7b734d2a7db1776973c654f4f2ee67dcb7
Shubham-Bhandari1 <shubhambhndari@gmail.com>
2026-07-15 20:55:26 +0530

    Simple Projects

---
+++ Project/Dice-Game/dice.js
@@ -0,0 +1,14 @@
+var randomnumber1=Math.floor(Math.random()*6)+1;
+var randomnumber2=Math.floor(Math.random()*6)+1;
+
+document.querySelector(".img1").setAttribute("src","images/dice"+randomnumber1+".png");
+document.querySelector(".img2").setAttribute("src","images/dice"+randomnumber2+".png");
+
+if(randomnumber1>randomnumber2){
+    document.querySelector("h2").innerHTML="Player 1 Wins";
+}else if(randomnumber1<randomnumber2){
+    document.querySelector("h2").innerHTML="Player 2 Wins";
+
+}else{
+    document.querySelector("h2").innerHTML="Draw hogya reee";
+}
